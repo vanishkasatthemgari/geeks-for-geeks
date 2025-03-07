985ec88a714d94cf0d8296d1d4f4c254ae9691ed
vanishka <149815574+vanishkasatthemgari@users.noreply.github.com>
2025-03-07 23:45:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Array Duplicates/array-duplicates.py
@@ -0,0 +1,26 @@
+from collections import Counter
+class Solution:
+    def findDuplicates(self, arr):
+        # code here
+        return [k for k,v in Counter(arr).items() if v>1]      
+
+
+
+#{ 
+ # Driver Code Starts
+# Initial Template for Python 3
+
+t = int(input())  # number of test cases
+for _ in range(t):
+    arr = list(map(int, input().split()))  # input array
+    s = Solution().findDuplicates(arr)  # find the duplicates
+    # Sort the result in ascending order
+    s.sort()
+    # Output formatting
+    if s:
+        print(" ".join(map(str, s)))  # Print duplicates in ascending order
+    else:
+        print("[]")  # Print empty list if no duplicates are found
+    print("~")
+
+# } Driver Code Ends
